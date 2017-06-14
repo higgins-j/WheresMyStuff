54248ca9715ee49ba0907f87da0a97d9b11c6ad3
just1higg <just1higg@yahoo.com>
2017-06-14 01:47:14 -0400

    Package moves and basic UI creation -Moved MainActivity.java to a controllers package. -Improved documentation on Login.java -Created basic UI elements in content_main.xml for data entry

--- app/src/main/java/edu/gatech/cs2340/wheresmystuff/model/Login.java
+++ app/src/main/java/edu/gatech/cs2340/wheresmystuff/model/Login.java
@@ -1,9 +1,21 @@
 package edu.gatech.cs2340.wheresmystuff.model;
 
+/**
+ * @author Justin Higgins
+ * @version 1.0
+ *
+ * This class will be used to check login information.
+ */
 public class Login {
     private static final String username = "username";
     private static final String password = "password";
 
+    /**
+     *
+     * @param user username entered by the user in the login screen
+     * @param pass password entered by the user in the login screen
+     * @return true if credentials are correct, false if they are incorrect
+     */
     public boolean check(String user, String pass) {
         return (user.equals(username) && pass.equals(password));
     }
