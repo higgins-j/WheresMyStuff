13d157c43d8f3a46364f7c190725d2469d4182ef
Monira Khan <monirakhan21@gmail.com>
2017-06-20 22:16:12 -0400

    Made basic Admin.java class

---
+++ app/src/main/java/edu/gatech/cs2340/wheresmystuff/model/Admin.java
@@ -0,0 +1,55 @@
+package edu.gatech.cs2340.wheresmystuff.model;
+
+/**
+ * @author Monira Khan
+ * @version 1.0
+ */
+
+ /**
+  * This class represents an Admin
+  */
+
+ public class Admin extends User {
+    public Admin(String user, String pass, String name, String phoneNum,
+        String address, boolean accountState) {
+        super(user, pass, name, phoneNum, address, accountState);
+    }
+
+    public Admin(String user, String pass) {
+        super(user, pass);
+    }
+
+    /**
+     * Removes inappropriate posts
+     * @return [description]
+     */
+    public boolean removePost() {
+        return false;
+    }
+
+    /**
+     * Bans user
+     * @param  bannedUser [description]
+     * @return            [description]
+     */
+    public boolean banUser(User bannedUser) {
+        return false;
+    }
+
+    /**
+     * Makes user into an Admin
+     * @param toBeAdmin [description]
+     */
+    public void userToAdmin(User toBeAdmin) {
+
+    }
+
+    /**
+     * Unlocks a user's account
+     * @param unLocked [description]
+     */
+    public void unLockAccount(User unLocked) {
+        unLocked.setAccountState(true);
+    }
+
+ }
