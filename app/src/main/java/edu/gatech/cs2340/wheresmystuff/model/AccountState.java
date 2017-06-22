cccf55ad042c8e8cd53b6493424671b2bef5a45a
Monira Khan <monirakhan21@gmail.com>
2017-06-22 15:10:10 -0400

    added a missing javadoc

--- app/src/main/java/edu/gatech/cs2340/wheresmystuff/model/AccountState.java
+++ app/src/main/java/edu/gatech/cs2340/wheresmystuff/model/AccountState.java
@@ -5,6 +5,9 @@ package edu.gatech.cs2340.wheresmystuff.model;
  * @version 1.0
  */
 
+/**
+ * Represents the account state of a user's account
+ */
 public enum AccountState {
     ACTIVE, INACTIVE, BANNED, DELETED;
 }
