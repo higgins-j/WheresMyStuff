e252d072cbbabda070ee8fbdfb53f08f6159dbcf
skipkayhil <skipkayhil@gmail.com>
2017-06-29 03:07:40 -0400

    added ListView to App, relocated Logout button to menu - removed logout button, replaced with menu item on toolbar - added listview, populated by Firebase database

--- app/src/main/java/edu/gatech/cs2340/wheresmystuff/controller/App.java
+++ app/src/main/java/edu/gatech/cs2340/wheresmystuff/controller/App.java
@@ -5,14 +5,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import edu.gatech.cs2340.wheresmystuff.R;
 
 public class App extends AppCompatActivity {
+
+    DatabaseReference mItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +33,58 @@ public class App extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_item);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AddItemActivity.class);
                 startActivity(intent);
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
             }
         });
-        Button logoutButton = (Button) findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+
+        final ListView listView = (ListView) findViewById(R.id.list_items);
+        final ArrayList<String> itemList = new ArrayList<>();
+
+        mItems = FirebaseDatabase.getInstance().getReference().child("items");
+        mItems.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                finish();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                itemList.clear();
+                for (DataSnapshot item : dataSnapshot.getChildren()) {
+                    itemList.add((String) item.getValue());
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                        getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        itemList);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
     }
 
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//
-//        // Use this to restore anything we add to the action bar after fragment closes
-//        this.getSupportActionBar().setTitle("Where's My Stuff?");
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        getMenuInflater().inflate(R.menu.menu_app, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
